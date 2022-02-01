88ee8d90f497641633d9659e681ef971910ba622
sharmavins23 <sharmavins23@gmail.com>
2022-02-01 00:33:26 -0500

    added mock transactions

--- src/transaction.js
+++ src/transaction.js
@@ -1,10 +1,37 @@
 // ./src/transaction.js
 // * Contains the class definition for a single transaction.
 
+// Generates a random IPv4 address string as a mock address
+function generateRandomIPv4() {
+    let ipv4 = "";
+
+    // Create network part 1
+    ipv4 += Math.floor(Math.random() * 255) + 1;
+    ipv4 += ".";
+
+    // Create network part 2
+    ipv4 += Math.floor(Math.random() * 255) + 1;
+    ipv4 += ".";
+
+    // Create host part 1
+    ipv4 += Math.floor(Math.random() * 255) + 1;
+    ipv4 += ".";
+
+    // Create host part 2
+    ipv4 += Math.floor(Math.random() * 255) + 1;
+
+    return ipv4;
+}
+
+// Generates a random money amount as a mock amount
+function generateRandomMoney() {
+    return Math.floor(Math.random() * 1000000);
+}
+
 class Transaction {
     constructor(fromAddress = "", toAddress = "", amount = 0) {
-        this.fromAddress = fromAddress;
-        this.toAddress = toAddress;
+        this.fromAddress = generateRandomIPv4();
+        this.toAddress = generateRandomIPv4();
         this.amount = amount;
     }
 }
