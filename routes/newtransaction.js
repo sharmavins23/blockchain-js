9e04640ba2429aa5f8a34d121ddc85edb89f70a4
sharmavins23 <sharmavins23@gmail.com>
2022-02-01 00:37:23 -0500

    created a new transaction route

---
+++ routes/newtransaction.js
@@ -0,0 +1,25 @@
+// ./routes/newtransaction.js
+// * Creates a new mock transaction and adds it to the system
+
+// * Imports
+const Transaction = require("../src/transaction");
+
+function newtransaction(app) {
+    // Create a new transaction
+    app.get("/newtransaction", function (request, response) {
+        // Create a new Transaction object
+        let tx = new Transaction();
+
+        // Add the transaction to the global transactions array
+        global.transactions.push(tx);
+
+        // Send the response for creating a new transaction
+        response
+            .status(200) // HTTP status code 200: OK
+            .send(
+                `Host ${tx.fromAddress} sent ${tx.toAddress} \$${tx.amount}.`
+            ); // Response message
+    });
+}
+
+module.exports = newtransaction;
