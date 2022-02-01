fa9dd6ec615963654ed4d2d268b8a3d6e4af7145
sharmavins23 <sharmavins23@gmail.com>
2022-01-31 22:38:33 -0500

    added server listening

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ const morgan = require("morgan"); // Imports Morgan's class definition
 
 // Initialize express's class object
 const app = express();
+// Tell Express to use Morgan for logging requests to the console
+app.use(morgan("dev")); // Pretty-print requests with the "dev" format
 
 // Create the port number for the server to listen on
 const port = 8080; // See: Wikipedia's List of TCP and UDP port numbers
 
 // Dynamically load all routes from the ./routes folder
 require("./routes")(app);
+
+// Configure our server to run
+app.listen(port, () => {
+    // Log that our server is running in the terminal
+    console.log(`Server is listening at http://localhost:${port}/`);
+});
