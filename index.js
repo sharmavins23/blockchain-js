abdae42b761ac285dc9c16f7665193eb0900cf02
sharmavins23 <sharmavins23@gmail.com>
2022-01-31 22:25:30 -0500

    added routing files

---
+++ index.js
@@ -0,0 +1,9 @@
+// * Imports
+const express = require("express"); // Imports Express's class definition
+const morgan = require("morgan"); // Imports Morgan's class definition
+
+// Initialize express's class object
+const app = express();
+
+// Create the port number for the server to listen on
+const port = 8080; // See: Wikipedia's List of TCP and UDP port numbers
---
+++ routes/index.js
@@ -0,0 +1,27 @@
+// ./routes/index.js
+// * Dynamically loads route files included in this folder.
+
+// * Imports
+const fs = require("fs");
+
+function dynamicallyLoadRoutes(app) {
+    // Read all of the filenames in the current folder, then apply this function
+    //  to each of them
+    fs.readdirSync(__dirname).forEach(function (file) {
+        // Make sure we skip this file, as it's not a route!
+        //  Also skip all non JS files!
+        if (
+            file === "index.js" ||
+            file.substr(file.lastIndexOf(".") + 1) !== "js"
+        )
+            return;
+
+        // Let's grab the name of the file
+        let name = file.substr(0, file.indexOf("."));
+        // Add the routes file to the exports
+        require("./" + name)(app);
+    });
+}
+
+// Export this function to dynamically load routes from the files in the folder
+module.exports = dynamicallyLoadRoutes;
